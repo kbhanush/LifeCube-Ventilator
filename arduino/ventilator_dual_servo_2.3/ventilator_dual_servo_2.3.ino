d770a16ef9e588dac09c4d439a91d1e7436e71cb
Kris Bhanushali <kris.bhanushali@oracle.com>
2020-07-09 09:18:13 -0700

    init

---
+++ arduino/ventilator_dual_servo_2.3/ventilator_dual_servo_2.3.ino
@@ -0,0 +1,195 @@
+ /* Latest code with iOS bluetooth support and direct string transfer from ionic 
+
+*/
+
+#include <Servo.h>
+
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];     
+char messageFromPC[numChars] = {0};
+int integerFromPC = 0;
+int respirationRate = 0;
+int tVolume = 0;
+int pauseBB = 0;
+float floatFromPC = 0.0;
+
+boolean newData = false;
+
+Servo myservoR; // Right side servo marked 'R' on motor
+Servo myservoL;  // Left side servo marked 'L' on motor
+// Note that each servo has a different initial position due to how the motors are attached
+//and how they initialize with the arms wide apart
+
+int ServoR_pos = 0; // This determines right arms actual position at any given time
+int ServoL_pos = 0;    // This determines left arms actual position at any given time
+
+
+int initialServoR = 30; // Right arm starts at rightmost position. This is the max it goes out
+
+
+
+int ServoR_max = 5; //ServoR_max mean how close to the center the right servo arm gets. 
+                    //5 means perpendicular this should not go below 5. 
+                    //Increase this for lighter breaths
+
+int initialServoL = 0; //Left arm starts at leftmost position. This is the max it goes out
+int ServoL_max = 25; //ServoL_max mean how close to the center the left servo arm gets.
+                     // X means perpendicular. This should no go above this X
+                     // Decrease this propotionate to ServoR_max for lighter breaths
+                      
+int rate = 60;
+int pause_after_inhale = 1000;
+int pause_after_exhale = 1000;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("<Arduino is ready for ya!>");
+  myservoR.attach(9);
+  myservoL.attach(11);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+
+    recvWithStartEndMarkers();
+    if (newData == true) {
+        strcpy(tempChars, receivedChars);
+            // this temporary copy is necessary to protect the original data
+            //   because strtok() used in parseData() replaces the commas with \0
+        parseData();
+        showParsedData();
+        newData = false;
+    }
+  
+  for (ServoR_pos = initialServoR, ServoL_pos = initialServoL; ServoR_pos >= ServoR_max && ServoL_pos <= ServoL_max; ServoR_pos -= 1, ServoL_pos += 1) 
+  { 
+    myservoR.write(ServoR_pos);   
+    myservoL.write(ServoL_pos);  //            
+    delay(rate);
+  }
+
+  delay(pause_after_inhale);
+  for (ServoR_pos = ServoR_max, ServoL_pos = ServoL_max; ServoR_pos <= initialServoR && ServoL_pos >= initialServoL; ServoR_pos += 1, ServoL_pos -= 1) 
+  {
+    myservoR.write(ServoR_pos);
+    myservoL.write(ServoL_pos); //
+    delay(rate);                       
+  }
+  
+ delay(pause_after_exhale);
+}
+
+
+void updateData() {
+  
+    Serial.println(respirationRate);
+    Serial.println(tVolume);
+    Serial.println(pauseBB);
+
+    switch (respirationRate) {
+      case 6:
+        rate = 150;
+        break;
+      case 7:
+        rate = 125;
+        break;
+      case 8:
+        rate = 105;
+        break;
+      case 9:
+        rate = 90;
+        break;
+      case 10: 
+        rate = 80;
+        break;
+      case 11:
+        rate = 70;
+        break;
+      case 12:
+        rate = 60;
+        break;
+      case 13:
+        rate = 50;
+        break;
+      case 14:
+        rate = 40;
+        break;
+      case 15:
+        rate = 40;
+        break;                                 
+    }
+    switch (tVolume) {
+      case 3:
+        ServoR_max = 2;
+        ServoL_max = 28;
+        break;
+      case 2:
+        ServoR_max = 10;
+        ServoL_max = 20;
+        break;
+      case 1:
+        ServoR_max = 15;
+        ServoL_max =  15;
+        break;  
+    }
+ 
+    pause_after_exhale = pauseBB * 1000;
+}
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void parseData() {      // split the data into its parts
+
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(tempChars,",");      // get the first part - the string
+    //strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
+    respirationRate = atoi(strtokIndx);
+ 
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    tVolume = atoi(strtokIndx);     // convert this part to an integer
+
+    strtokIndx = strtok(NULL, ",");
+    pauseBB = atoi(strtokIndx);     // convert this part to a float
+    
+}
+
+void showParsedData() {
+    Serial.print("respirationRate: ");
+    Serial.println(respirationRate);
+    Serial.print("tVolume: ");
+    Serial.println(tVolume);
+    Serial.print("pause b/w breath: ");
+    Serial.println(pauseBB);
+
+    updateData();
+}
---
+++ arduino/ventilator_dual_servo_2.3/ventilator_dual_servo_2.4/ventilator_dual_servo_2.4.ino
@@ -0,0 +1,211 @@
+
+ 
+#include <Servo.h>
+ 
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];    
+char messageFromPC[numChars] = {0};
+int integerFromPC = 0;
+int respirationRate = 0;
+int tVolume = 0;
+int pauseBB = 0;
+int power = 1;
+float floatFromPC = 0.0;
+ 
+boolean newData = false;
+ 
+Servo myservoR; // Right side servo marked 'R' on motor
+Servo myservoL;  // Left side servo marked 'L' on motor
+// Note that each servo has a different initial position due to how the motors are attached
+//and how they initialize with the arms wide apart
+ 
+int ServoR_pos = 0; // This determines right arms actual position at any given time
+int ServoL_pos = 0;    // This determines left arms actual position at any given time
+ 
+
+int initialServoL = 5; //Left arm starts at leftmost position. This is the max it goes out  
+int initialServoR = 40; // Right arm starts at rightmost position. This is the max it goes out
+ 
+ 
+int ServoL_max = 37; //ServoL_max mean how close to the center the left servo arm gets.
+                     // 37 means perpendicular. This should no go above this 37
+                     // Decrease this propotionate to ServoR_max for lighter breaths
+int ServoR_max = 7; //ServoR_max mean how close to the center the right servo arm gets.
+                    //7 means perpendicular this should not go below 7.
+                    //Increase this for lighter breaths
+ 
+
+
+                     
+int rate = 40;
+int pause_after_inhale = 500;
+int pause_after_exhale = 500;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("<Arduino is ready>");
+  myservoR.attach(9);
+  myservoL.attach(11);  // attaches the servo on pin 9 to the servo object
+}
+ 
+void loop() {
+ 
+    recvWithStartEndMarkers();
+    if (newData == true) {
+        strcpy(tempChars, receivedChars);
+            // this temporary copy is necessary to protect the original data
+            //   because strtok() used in parseData() replaces the commas with \0
+        parseData();
+        showParsedData();
+        newData = false;
+    }
+ 
+  if (power == 1) {
+     
+      for (ServoR_pos = initialServoR, ServoL_pos = initialServoL; ServoR_pos >= ServoR_max && ServoL_pos <= ServoL_max; ServoR_pos -= 1, ServoL_pos += 1)
+      {
+        myservoR.write(ServoR_pos);  
+        myservoL.write(ServoL_pos);  //            
+        delay(rate);
+      }
+   
+      delay(pause_after_inhale);
+      for (ServoR_pos = ServoR_max, ServoL_pos = ServoL_max; ServoR_pos <= initialServoR && ServoL_pos >= initialServoL; ServoR_pos += 1, ServoL_pos -= 1)
+      {
+        myservoR.write(ServoR_pos);
+        myservoL.write(ServoL_pos); //
+        delay(rate);                      
+      }
+     
+     delay(pause_after_exhale);
+ 
+  }
+}
+ 
+ 
+void updateData() {
+ 
+    Serial.println(respirationRate);
+    Serial.println(tVolume);
+    Serial.println(pauseBB);
+ 
+    switch (respirationRate) {
+      case 6:
+        rate = 150;
+        break;
+      case 7:
+        rate = 125;
+        break;
+      case 8:
+        rate = 105;
+        break;
+      case 9:
+        rate = 90;
+        break;
+      case 10:
+        rate = 80;
+        break;
+      case 11:
+        rate = 70;
+        break;
+      case 12:
+        rate = 60;
+        break;
+      case 13:
+        rate = 50;
+        break;
+      case 14:
+        rate = 40;
+        break;
+      case 15:
+        rate = 40;
+        break;
+      case 30:
+        rate = 10;
+        break; 
+      case 40:
+        rate = 5;
+        break;                                     
+    }
+    switch (tVolume) {
+      case 3:                     // This was updated after re-calibration
+        ServoR_max = 7;
+        ServoL_max = 37;
+        break;
+      case 2:                     // This was updated after re-calibration
+        ServoR_max = 15;
+        ServoL_max = 30;
+        break;
+      case 1:                     // This was updated after re-calibration
+        ServoR_max = 20;   
+        ServoL_max =  23;
+        break;  
+    }
+ 
+    if (pauseBB >= 0 && pauseBB < 4) {
+      pause_after_exhale = pauseBB * 1000;
+    }
+}
+ 
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+ 
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+ 
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+ 
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+ 
+void parseData() {      // split the data into its parts
+ 
+    char * strtokIndx; // this is used by strtok() as an index
+ 
+    strtokIndx = strtok(tempChars,",");      // get the first part - the string
+    //strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
+    power = atoi(strtokIndx);
+ 
+    strtokIndx = strtok(NULL, ",");
+    respirationRate = atoi(strtokIndx);
+ 
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    tVolume = atoi(strtokIndx);     // convert this part to an integer
+ 
+    strtokIndx = strtok(NULL, ",");
+    pauseBB = atoi(strtokIndx);     // convert this part to a float
+   
+}
+ 
+void showParsedData() {
+    Serial.print("respirationRate: ");
+    Serial.println(respirationRate);
+    Serial.print("tVolume: ");
+    Serial.println(tVolume);
+    Serial.print("pause b/w breath: ");
+    Serial.println(pauseBB);
+    Serial.print("power:");
+    Serial.println(power);
+    updateData();
+}
